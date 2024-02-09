f74ff64c50a6fc9674b9e8658bd1a34f1af30ec1
Arne <arne.schittenhelm@web.de>
2024-02-10 00:07:03 +0100

    test commit

--- main/Main.java
+++ main/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Hallo
+		
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
